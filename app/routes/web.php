e0f9a371ec18943b523d6a9f85fcbde8ef984bdc
miyu0903 <2525.canpass@gmail.com>
2023-10-14 17:06:32 +0900

    comicomi

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,8 +22,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//add
+//ここから追加
 Route::resource('todo', 'App\Http\Controllers\TodoController');
 Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
 Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
-//add
+//ここまで追加
